efa2357af7256a71445ae1118a0d7c36d13119ba
Iurii Lopatenko <yalopatenko@gmail.com>
2022-12-02 22:59:50 -0800

    Chapter 3. Lesson 2 is finished
    Basic Styling

---
+++ feedback-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header';
+const App = () => (
+  <>
+    <Header />
+    <div className='container'>
+      <h1>My app</h1>
+    </div>
+  </>
+);
+
+export default App;
