75b7bc15825069051c5176831b511c384dd860aa
Iurii Lopatenko <yalopatenko@gmail.com>
2022-12-03 13:38:28 -0800

    Chapter 3. Lesson 5
    Conditional classes and styling

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import Header from './components/Header';
-const App = () => (
-  <>
-    <Header />
-    <div className='container'>
-      <h1>My app</h1>
-    </div>
-  </>
-);
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 
+const App = () => {
+  const [feedback, setFeedback] =
+    useState(FeedbackData);
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
+  );
+};
 export default App;
---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,28 @@
+const FeedbackData = [
+  {
+    id: 3245,
+    rating: 4,
+    text: 'This is a feedback with ID 3245',
+  },
+  {
+    id: 3577,
+    rating: 6,
+    text: 'This is a feedback with ID 3577',
+  },
+  {
+    id: 1111,
+    rating: 2,
+    text: 'This is a feedback with ID 1111',
+  },
+  {
+    id: 7777,
+    rating: 1,
+    text: 'This is a feedback with ID 7777',
+  },
+  {
+    id: 9999,
+    rating: 9,
+    text: 'This is a feedback with ID 9999',
+  },
+];
+export default FeedbackData;
