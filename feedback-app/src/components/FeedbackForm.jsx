6a255a3e4e24f1b9c3f14435aa3a91958a2debad
Iurii Lopatenko <yalopatenko@gmail.com>
2022-12-04 11:51:37 -0800

    Chapter 4. Lesson 3
    Realtime vilidation - length of review message

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -5,15 +5,25 @@ import Button from './shared/Button';
 import { useState } from 'react';
 const FeedbackForm = props => {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
   const handleTextChange = e => {
+    if (text.length === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 characters');
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
   return (
     <Card>
       <form>
-        <h2>
-          How would yoy rate service with us?
-        </h2>
+        <h2>How would yoy rate service with us?</h2>
         {/*  TODO: rating select component*/}
         <div className='input-group'>
           <input
@@ -22,8 +32,11 @@ const FeedbackForm = props => {
             placeholder='Write a review'
             value={text}
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
