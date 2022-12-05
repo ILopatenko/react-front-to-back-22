c8fc758be47ff20b63f44adae2c0b9ddbe6f862a
Iurii Lopatenko <yalopatenko@gmail.com>
2022-12-04 16:17:07 -0800

    Chapter 5. Lesson 2
    Routes with React router 6

--- feedback-app/src/App.jsx
+++ feedback-app/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import AboutPage from './pages/AboutPage';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
-
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
   const deleteFeedback = id => {
@@ -20,18 +25,47 @@ const App = () => {
     feedbackObject.id = uuidv4();
     setFeedback([feedbackObject, ...feedback]);
   };
-  return (
-    <>
+  /*   return (
+    <Router>
       <Header />
       <div className='container'>
+        <Routes>
         <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList
           feedback={feedback}
           handleDelete={deleteFeedback}
         />
+        <AboutPage link to='' />
+      
+     
+    </Router>
+    </Routes>
+  ); */
+  //////
+  return (
+    <Router>
+      <Header />
+      <div className='container'>
+        <Routes>
+          <Route
+            exct
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          />
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 };
 export default App;
