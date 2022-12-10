7a54aef0f0408a85b8e62a562c6124f15377ec11
Iurii Lopatenko <yalopatenko@gmail.com>
2022-12-10 12:12:05 -0800

    CH9 L2
    UserList component. First fetch API request to GitHub API with a token

--- github-finder/src/pages/Home.jsx
+++ github-finder/src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import UserResults from '../components/users/UserResults';
 const Home = props => {
    return (
       <div>
          <h1 className='text-6xl mb-4'>Welcome</h1>
-         {process.env.REACT_APP_GITHUB_TOKEN}
+         {/* TODO: SEARCH COMPONENT */}
+         <UserResults />
       </div>
    );
 };
