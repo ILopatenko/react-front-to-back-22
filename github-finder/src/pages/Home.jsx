8e6907270f2e0c2a8eb883e5b95da8e6818ce1c8
Iurii Lopatenko <yalopatenko@gmail.com>
2022-12-10 17:03:17 -0800

    CH9 L8
    UserSearch component

--- github-finder/src/pages/Home.jsx
+++ github-finder/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import UserResults from '../components/users/UserResults';
+import UserSearch from '../components/users/UserSearch';
 const Home = props => {
    return (
       <div>
          <h1 className='text-6xl mb-4'>Welcome</h1>
-         {/* TODO: SEARCH COMPONENT */}
+         <UserSearch />
          <UserResults />
       </div>
    );
