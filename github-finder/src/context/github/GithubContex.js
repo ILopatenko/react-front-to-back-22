068205b6706281cb2c0240af69e05638eafa062a
Iurii Lopatenko <yalopatenko@gmail.com>
2022-12-11 16:52:31 -0800

    CH10 L6
    Get User Repos

--- github-finder/src/context/github/GithubContex.js
+++ github-finder/src/context/github/GithubContex.js
@@ -42,7 +42,11 @@ export const GithubProvider = ({ children }) => {
    };
    const getUserRepos = async login => {
       setLoading();
-      const response = await fetch(`${GITHUB_URL}/users/${login}/repos`, {
+      const params = new URLSearchParams({
+         sort: 'created',
+         per_page: 10,
+      });
+      const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
          headers: {
             Authorization: `token ${GITHUB_TOKEN}`,
          },
